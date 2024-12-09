f7d56af427ebff23b8d1efa820bfbaadebd3a39c
TseManFung <163531908+TseManFung@users.noreply.github.com>
2024-12-09 22:27:52 +0800

    scan loop

--- app/src/main/java/com/crms/crmsAndroid/TestRFID.kt
+++ app/src/main/java/com/crms/crmsAndroid/TestRFID.kt
@@ -1,21 +1,25 @@
 package com.crms.crmsAndroid
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import android.widget.Spinner
 import androidx.activity.ComponentActivity
 import com.crms.crmsAndroid.scanner.rfidScanner
-import com.rscja.deviceapi.RFIDWithUHFUART
-import com.rscja.deviceapi.entity.*
-import com.rscja.deviceapi.interfaces.IUHF
+import com.rscja.deviceapi.entity.UHFTAGInfo
 
 class TestRFID : ComponentActivity() {
     private lateinit var lvMain: ListView
     private lateinit var btnScan: Button
+    private lateinit var btnStop: Button
     private lateinit var spnBank: Spinner
     private val items = mutableListOf<String>() // 用於存放 ListView 的數據
+    private lateinit var listAdapter: ArrayAdapter<String>
+    var objRfidScanner: rfidScanner = rfidScanner()
     init {
         System.loadLibrary("IGLBarDecoder") // 加載 libIGLBarDecoder.so
         System.loadLibrary("IGLImageAE")    // 加載 libIGLImageAE.so
@@ -27,15 +31,19 @@ class TestRFID : ComponentActivity() {
 
         lvMain = findViewById(R.id.lvMain)
         btnScan = findViewById(R.id.btnScan)
+        btnStop = findViewById(R.id.btnStop)
         spnBank = findViewById(R.id.spnBank)
 
         // 設定 ListView 的適配器
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
-        lvMain.adapter = adapter
+        listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        lvMain.adapter = listAdapter
 
         // 設定按鈕點擊事件
         btnScan.setOnClickListener {
-            handleBtnScanClick(adapter)
+            handleBtnScanClick()
+        }
+        btnStop.setOnClickListener {
+            objRfidScanner.stopReadTagLoop()
         }
 
         // 設定 Spinner 的適配器
@@ -47,45 +55,100 @@ class TestRFID : ComponentActivity() {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spnBank.adapter = adapter
         }
+
+        appendTextToList("RFID 版本: ${objRfidScanner.getVersion()} | set Mode: ${this.objRfidScanner.setMode()}") // 獲取 RFID 版本
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (objRfidScanner != null) {
+            objRfidScanner.free()
+        }
     }
 
     // 處理 btnScan 按鈕的點擊事件
-    private fun handleBtnScanClick(adapter: ArrayAdapter<String>) {
-        var rfidData = "無法掃描到 RFID 訊號"
-        var sc:rfidScanner? = null
-        try {
-            sc = rfidScanner()
-            val scanner = sc.getScanner();
-            val tag = scanner.inventorySingleTag()
-            tag.reserved = sc.readTag(0)
-            tag.tid = sc.readTag(2)
-            tag.user = sc.readTag(3)
-            if (tag != null) {
-                rfidData = """ |EPC: ${tag.epc} |TID: ${tag.tid} |RSSI: ${tag.rssi} |Antenna: ${tag.ant} |Index: ${tag.index} |PC: ${tag.pc} |Remain: ${tag.remain} |Reserved: ${tag.reserved} |User: ${tag.user} """.trimMargin()
+    private fun handleBtnScanClick() {
+        var rfidData = "---"
 
-            }else{
-                rfidData = "無法掃描到 RFID 訊號"
+        try {
+            // read single tag
+//            objRfidScanner = rfidScanner()
+//            val scanner = objRfidScanner.scanner;
+//            val tag = scanner.inventorySingleTag()
+//            tag.reserved = objRfidScanner.readTag(0)
+//            tag.tid = objRfidScanner.readTag(2)
+//            tag.user = objRfidScanner.readTag(3)
+//            if (tag != null) {
+//                rfidData =
+//                    """ |EPC: ${tag.epc} |TID: ${tag.tid} |RSSI: ${tag.rssi} |Antenna: ${tag.ant} |Index: ${tag.index} |PC: ${tag.pc} |Remain: ${tag.remain} |Reserved: ${tag.reserved} |User: ${tag.user} """.trimMargin()
+//            } else {
+//                rfidData = "無法掃描到 RFID 訊號"
+//            }
+
+            val handler1: Handler = object : Handler() {
+                override fun handleMessage(msg: Message) {
+                    appendTextToList(msg.obj.toString())
+                }
             }
+            objRfidScanner.readTagLoop()
+            readLoopForTest(objRfidScanner, handler1).start()
+
         } catch (e: Exception) {
             rfidData = e.message.toString() // 捕獲異常
-        }finally {
-            if(sc != null){
-                sc.free()
-            }
+        } finally {
+//            if (objRfidScanner != null) {
+//                objRfidScanner.free()
+//            }
         }
 
-        val newText = "新掃描項目 $rfidData"
-        appendTextToList(newText, adapter)
+        val newText = "end line: $rfidData"
+        appendTextToList(newText)
 
     }
 
     // 將文本追加到 ListView
-    private fun appendTextToList(text: String, adapter: ArrayAdapter<String>) {
+    fun appendTextToList(text: String) {
         items.add(text) // 將新文本添加到列表中
-        adapter.notifyDataSetChanged() // 通知適配器數據已更新
+        listAdapter.notifyDataSetChanged() // 通知適配器數據已更新
+    }
+    open class readLoopForTest(private val objRfidScanner: rfidScanner, private val handler: Handler) : Thread() {
+        override fun run() {
+            val msg = handler.obtainMessage()
+            msg.obj = "start"
+            handler.sendMessage(msg)
+            while (objRfidScanner.loopFlag){
+                val tag = objRfidScanner.scanner.readTagFromBuffer()
+                if (tag != null) {
+                    val msg = handler.obtainMessage()
+                    msg.obj = """ |EPC: ${tag.epc} |TID: ${tag.tid} |RSSI: ${tag.rssi} |Antenna: ${tag.ant} |Index: ${tag.index} |PC: ${tag.pc} |Remain: ${tag.remain} |Reserved: ${tag.reserved} |User: ${tag.user} """.trimMargin()
+                    handler.sendMessage(msg)
+                }else{
+                    try {
+                        Thread.sleep(100)
+                    }catch (e: InterruptedException){
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
     }
 
+//    this is a sample code for readLoop in real project
+//    class readLoop(private val objRfidScanner: rfidScanner): Thread() {
+//        init {
+//            this.start()
+//        }
+//        override fun run(){
+//            while (objRfidScanner.loopFlag){
+//                val tag = objRfidScanner.scanner.readTagFromBuffer()
+//                // do something with tag
+//            }
+//        }
+//    }
+
 }
+
 fun byteArrayToHexString(byteArray: ByteArray): String {
     return byteArray.joinToString("") { String.format("%02X", it) }
 }
--- app/src/main/java/com/crms/crmsAndroid/scanner/rfidScanner.kt
+++ app/src/main/java/com/crms/crmsAndroid/scanner/rfidScanner.kt
@@ -1,14 +1,17 @@
 package com.crms.crmsAndroid.scanner
 
+import android.os.Looper
 import com.rscja.deviceapi.RFIDWithUHFUART
 import com.rscja.deviceapi.interfaces.ConnectionStatus
 
 class rfidScanner {
-    private lateinit var scanner: RFIDWithUHFUART
+    lateinit var scanner: RFIDWithUHFUART
+        private set
     private var password: String = "12345678"
     private val defaultPassword = "00000000"
     private var filterBank: Int? = null
     private var filterData: String? = null
+    var loopFlag = false
 
     init {
         this.scanner = RFIDWithUHFUART.getInstance();
@@ -16,15 +19,15 @@ class rfidScanner {
         if (!isInit) {
             throw Exception("Failed to initialize RFID scanner");
         }
-        this.scanner.setEPCAndTIDUserModeEx(2, 0, 6, 0, 2)
+//        this.scanner.setEPCAndTIDUserModeEx(2, 0, 6, 0, 2)
+    }
+
+    fun setMode(): Boolean {
+        return this.scanner.setEPCAndTIDUserMode(0, 2)
     }
 
     fun free(): Boolean {
         return this.scanner.free();
-    }
-
-    fun getScanner(): RFIDWithUHFUART {
-        return this.scanner
     }
 
     fun setPassword(password: String) {
@@ -49,21 +52,25 @@ class rfidScanner {
                 ptr = 0
                 cnt = 4
             }
+
             1 -> {
                 // EPC
                 ptr = 2
                 cnt = 6
             }
+
             2 -> {
                 // TID
                 ptr = 0
                 cnt = 6
             }
+
             3 -> {
                 // User
                 ptr = 0
                 cnt = 2
             }
+
             else -> {
                 throw IllegalArgumentException("Invalid bank value")
             }
@@ -71,6 +78,7 @@ class rfidScanner {
 
         return Pair(ptr, cnt)
     }
+
     private fun bankErrorMsg(bank: Int): String {
         when (bank) {
             0 -> return "Reserved Bank"
@@ -80,6 +88,7 @@ class rfidScanner {
             else -> return "Bank $bank"
         }
     }
+
     fun readTag(readBank: Int): String {
         val (ptr, cnt) = makePtrAndCnt(readBank)
 
@@ -100,11 +109,12 @@ class rfidScanner {
         this.filterBank = filterBank
         this.filterData = filterData
     }
-    fun readTagWithFilter(readBank:Int,filterBank:Int,filterData:String):String{
-        if (filterBank == 0){
+
+    fun readTagWithFilter(readBank: Int, filterBank: Int, filterData: String): String {
+        if (filterBank == 0) {
             throw IllegalArgumentException("Invalid filter bank value, filter bank cannot be Reserved Bank")
         }
-        this.setFilter(filterBank,filterData)
+        this.setFilter(filterBank, filterData)
         return this.readTagWithFilter(readBank)
     }
 
@@ -114,17 +124,46 @@ class rfidScanner {
         }
         val (readPtr, readCnt) = makePtrAndCnt(readBank)
         val (filterPtr, filterCnt) = makePtrAndCnt(filterBank!!)
-        var TagData = this.scanner.readData(password,filterBank!!,filterPtr,filterCnt,filterData!!,readBank,readPtr,readCnt)
+        var TagData = this.scanner.readData(
+            password,
+            filterBank!!,
+            filterPtr,
+            filterCnt,
+            filterData!!,
+            readBank,
+            readPtr,
+            readCnt
+        )
         if (TagData != null) {
             return TagData
         } else {
-            TagData = this.scanner.readData(defaultPassword,filterBank!!,filterPtr,filterCnt,filterData!!,readBank,readPtr,readCnt)
+            TagData = this.scanner.readData(
+                defaultPassword,
+                filterBank!!,
+                filterPtr,
+                filterCnt,
+                filterData!!,
+                readBank,
+                readPtr,
+                readCnt
+            )
             if (TagData != null) {
                 return TagData
             } else {
                 throw Exception("Failed to read tag from ${bankErrorMsg(readBank)}")
             }
         }
+    }
+
+    fun readTagLoop() {
+        // only can read EPC / Tid
+        this.scanner.startInventoryTag()
+        this.loopFlag = true
+    }
+
+    fun stopReadTagLoop() {
+        this.loopFlag = false
+        this.scanner.stopInventory()
     }
 
 //    pri fun lockTag{
