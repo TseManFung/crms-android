7baca860ca8ea208d7a204ea1f460f9126fb8b5e
Eric Ho <eric59135334@gmail.com>
2024-12-21 04:14:56 +0800

    testing

--- app/src/main/java/com/crms/crmsAndroid/ui/manInventory/manInventoryViewModel.kt
+++ app/src/main/java/com/crms/crmsAndroid/ui/manInventory/manInventoryViewModel.kt
@@ -1,13 +1,13 @@
-package com.crms.crmsAndroid.ui.home
+package com.crms.crmsAndroid.ui.manInventory
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class manInventoryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is manInventoryman Fragment"
     }
     val text: LiveData<String> = _text
 }
